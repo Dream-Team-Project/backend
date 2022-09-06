8c560b158a8cf60a94510555a3737d69bf652224
Dream-Team-Project <abhishek@dreamreflectionmedia.com>
2022-09-06 21:40:20 +0530

    live server

--- routes/db.js
+++ routes/db.js
@@ -1,13 +1,29 @@
 var mysql = require('mysql');
 // Database connection
 
+// const db = mysql.createConnection({
+//     host:'mean-test1.cjibinp9bsxg.ap-south-1.rds.amazonaws.com',
+//     user:'admindb',
+//     password: 'Admin1202',
+//     database: 'mean-test1',
+//     port: 3306,
+// });
+
 const db = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password: '',
-    database: 'angular_db',
+    host:'keabuilderdb.cjibinp9bsxg.ap-south-1.rds.amazonaws.com',
+    user:'keaadmin',
+    password: 'A^uUI09FeMe',
+    database: 'keabuilder_db',
     port: 3306,
 });
+
+// const db = mysql.createConnection({
+//     host:'localhost',
+//     user:'root',
+//     password: '',
+//     database: 'angular_db',
+//     port: 3306,
+// });
 
 // check database connection
 
